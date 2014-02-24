8d6b78e463bf462b31fb287649539ddc2fbb3f1f
Brendan Loudermilk <brendan@gophilosophie.com>
2014-02-23 17:04:13 -0800

    Hardcode pool size

--- lib/github_recommender/scraper.rb
+++ lib/github_recommender/scraper.rb
@@ -2,25 +2,17 @@ module GithubRecommender
   class Scraper < Celluloid::SupervisionGroup
     ONE_HOUR = 60 * 60
 
-    def self.pool_env(actor, options = {})
-      env = ENV["#{actor.name.underscore.upcase}_POOL"]
-      size = env ? env.to_i : 2
-
-      pool(actor, options.merge(size: size))
-    end
-
-    pool_env ArchiveDownloader, as: :archive_downloader
-    pool_env ArchiveProcessor, as: :archive_processor
+    pool ArchiveDownloader, as: :archive_downloader, size: 2
+    pool ArchiveProcessor, as: :archive_processor, size: 12
 
     def self.scrape!(start_at, end_at)
       run!
 
-      futures = []
       cursor = start_at
 
       until cursor > end_at
         callback = proc do |path|
-          futures << Celluloid::Actor[:archive_processor].future.process(path)
+          Celluloid::Actor[:archive_processor].async.process(path)
         end
 
         Celluloid::Actor[:archive_downloader].async.get(cursor, callback)
@@ -28,7 +20,7 @@ module GithubRecommender
         cursor += ONE_HOUR
       end
 
-      futures.each(&:value)
+      sleep
     end
   end
 end
